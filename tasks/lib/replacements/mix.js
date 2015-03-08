22abf29187ee8f1ff027bb024a9638c77c2c8b7f
Duvillier Antoine <duvillierantoine@gmail.com>
2015-03-08 12:12:50 -0700

    mix: improve regex

--- tasks/lib/replacements/mix.js
+++ tasks/lib/replacements/mix.js
@@ -2,7 +2,7 @@ module.exports = {
   pattern: /(shade|tint)\(([^,]+),\s?([\d%]+)/gi,
   replacement: function (match, method, color2, weight) {
     var color1 = method === 'shade' ? '#000000' : '#ffffff';
-    return 'mix(' + color1 + ', ' + color2 + ', ' + weight + ')';
+    return 'mix(' + [color1, color2, weight].join(', ') + ')';
   },
   order: 3
 };
