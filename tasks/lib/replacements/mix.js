6293aa26b98518e5f8c50226d3dd8543d86110c7
Duvillier Antoine <duvillierantoine@gmail.com>
2015-03-07 22:52:05 -0800

    mix: fix src color

--- tasks/lib/replacements/mix.js
+++ tasks/lib/replacements/mix.js
@@ -1,5 +1,8 @@
 module.exports = {
   pattern: /(shade|tint)+\((.*),\s?([\d%]+)\)/gmi,
-  replacement: 'mix(#000000, $2, $3)',
+  replacement: function (match, method, color2, weight) {
+    var color1 = method === 'shade' ? '#000000' : '#FFFFFF';
+    return 'mix(' + color1 + ', ' + color2 + ', ' + weight + ')';
+  },
   order: 3
 };
