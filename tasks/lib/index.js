000aba854663fc7b628d58bb2fef20f0ae3c2e80
Duvillier Antoine <duvillierantoine@gmail.com>
2015-03-07 20:41:54 -0800

    use native methods instead of lodash most as possible

--- tasks/lib/index.js
+++ tasks/lib/index.js
@@ -3,20 +3,15 @@ var fs = require('fs'),
   dir = __dirname + '/replacements/';
 
 var replacements = function (options) {
-  var filenames = fs.readdirSync(dir),
-    excludes = options.excludes,
-    customReplacements = options.replacements;
+  var filenames = fs.readdirSync(dir);
 
-  return _.chain(filenames)
-    .filter(function (filename) {
-      return excludes.indexOf(filename.split('.js').join('')) === -1;
-    })
-    .map(function (filename) {
-      return require(dir + filename);
-    })
-    .sortBy('order')
-    .value()
-    .concat(customReplacements);
+  var results = filenames.filter(function (filename) {
+    return options.excludes.indexOf(filename.split('.js').join('')) === -1;
+  }).map(function (filename) {
+    return require(dir + filename);
+  }).concat(options.replacements);
+
+  return _.sortBy(results, 'order');
 };
 
 module.exports = replacements;
