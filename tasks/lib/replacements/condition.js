378aeb918dff7f64a4a6e3044439481d10679331
Duvillier Antoine <duvillierantoine@gmail.com>
2015-03-07 22:34:34 -0800

    @include: fix condition

--- tasks/lib/replacements/condition.js
+++ tasks/lib/replacements/condition.js
@@ -13,6 +13,6 @@
  */
 module.exports = {
   pattern: /\.([\w-_]*)\s*\((.*)\)\s*when\s*\((.*)=(.*)\)\s*\{(\s*)([^}]+)}[;]?/gi,
-  replacement: '@if $3==$4 {$5@mixin $1($2){$5$6};};',
+  replacement: '@if $3==$4 {$5@mixin $1($2){$5$6}}',
   order: 1
 };
