bbef09f01da3ece29ed7836511bb59e40739f19f
Duvillier Antoine <duvillierantoine@gmail.com>
2015-03-08 20:02:34 -0700

    interpolation: allow more than once

--- tasks/lib/replacements/interpolation.js
+++ tasks/lib/replacements/interpolation.js
@@ -1,10 +1,11 @@
 /* README : http://sass-lang.com/documentation/file.SASS_REFERENCE.html#interpolation_
-    background: url("${img-buttons-dir};/loaders/default.gif");
+    background: url("${dir}/${path}");
   TO =====>
-    background: url('#{$img-buttons-dir};/loaders/default.gif');
+    background: url('#{$dir}/#{$path}');
 */
 module.exports = {
-  pattern: /\$\{(.+)\}/gi,
+  pattern: /\${([^}]+)\}/gi,
   replacement: '#{$$$1}',
   order: 3
 };
+// /\$\{([^}]+)\}
